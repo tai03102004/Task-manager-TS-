5876b1d0eb1e3d9e7f0ec076518367d9800b86ec
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2023-11-28 17:30:03 +0700

    update

---
+++ dist/api/v1/helpers/pagination.helper.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pagination = (objectPagination, query, countRecords) => {
+    if (query.page) {
+        objectPagination.currentPage = parseInt(query.page);
+    }
+    if (query.limit) {
+        objectPagination.limitItems = parseInt(query.limit);
+    }
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
+    objectPagination.totalPage = Math.ceil(countRecords / objectPagination.limitItems);
+    return objectPagination;
+};
+exports.default = pagination;
